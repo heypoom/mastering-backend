45242f24fa487e47aec9a89006e61767696cf67b
Phoomparin Mano <phoomparin@gmail.com>
2019-09-07 11:04:14 +0700

    :wrench: Add body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config'
 
 import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
 
 const {PORT = 8000} = process.env
 
 const app = express()
+
+app.use(bodyParser())
 
 app.get('/', (req: Request, res: Response) => {
   res.send({status: 'OK'})
