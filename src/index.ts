627be519fee0c8ce20ffc777a9cd01ee04e92140
Phoomparin Mano <phoomparin@gmail.com>
2019-09-07 11:10:03 +0700

    :sparkles: Add GET and POST /names

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,18 @@ app.get('/', (req: Request, res: Response) => {
   res.send({status: 'OK'})
 })
 
+const names: string[] = []
+
+app.get('/names', (req, res) => res.send({data: names}))
+
+app.post('/names', (req, res) => {
+  const {name} = req.body
+  if (!name) return res.status(400).send({error: 'Field `name` is required'})
+
+  names.push(name)
+  res.send({success: true})
+})
+
 app.listen(PORT, () => {
   console.log(`Server started at 0.0.0.0:${PORT}`)
 })
