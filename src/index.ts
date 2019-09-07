80edbdc761d7322405bd76280726485eaabcfada
Phoomparin Mano <phoomparin@gmail.com>
2019-09-07 10:52:32 +0700

    :sparkles: Blank Template

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import 'dotenv/config'
-
-import express, {Request, Response} from 'express'
-
-const {PORT = 8000} = process.env
-
-const app = express()
-
-app.get('/', (req: Request, res: Response) => {
-  res.send({status: 'OK'})
-})
-
-app.listen(PORT, () => {
-  console.log(`Server started at 0.0.0.0:${PORT}`)
-})
+console.log('Blank Template.')
