240b177bdd86cd5e40c8ce1d0230d0fd9f7bedc4
Phoomparin Mano <phoomparin@gmail.com>
2019-09-07 11:38:30 +0700

    :sparkles: Add device creation and retrieval endpoint

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,43 @@ const app = express()
 
 app.use(bodyParser())
 
+interface Device {
+  id: string
+  displayName: string
+  color: string
+  type: string
+}
+
+const devices: Device[] = []
+
 app.get('/', (req: Request, res: Response) => {
   res.send({status: 'OK'})
+})
+
+app.get('/devices', (req, res) => res.send({data: devices}))
+
+app.get('/devices/:id', (req, res) => {
+  const {id} = req.params
+  if (!id) return res.status(400).send({error: 'Device `id` is required.'})
+
+  const device = devices.find(d => d.id === id)
+  if (!device) return res.status(404).send({error: 'Device not found.'})
+
+  res.send({data: device})
+})
+
+app.post('/devices', (req, res) => {
+  const {id, displayName, color, type} = req.body
+
+  if (!id || !displayName || !color || !type) {
+    return res
+      .status(400)
+      .send({error: 'Field `id, displayName, color, type` are required.'})
+  }
+
+  devices.push({id, displayName, color, type})
+
+  res.send({success: true})
 })
 
 app.listen(PORT, () => {
