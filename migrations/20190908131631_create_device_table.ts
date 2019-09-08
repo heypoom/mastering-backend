49b519678f0dbb3d85a2316e3532ba3ca36940ae
Phoomparin Mano <phoomparin@gmail.com>
2019-09-08 13:34:48 +0700

    :sparkles:

---
+++ migrations/20190908131631_create_device_table.ts
@@ -0,0 +1,22 @@
+import * as Knex from 'knex'
+
+async function up(knex: Knex): Promise<any> {
+  const exist = await knex.schema.hasTable('devices')
+  if (exist) return
+
+  return knex.schema.createTable('devices', t => {
+    t.increments('id')
+    t.string('name')
+    t.string('state')
+    t.string('type')
+    t.string('location')
+    t.string('color')
+  })
+}
+
+async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable('devices')
+}
+
+exports.up = up
+exports.down = down
