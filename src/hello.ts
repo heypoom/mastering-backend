a786ee9ec98ae0b70fd19474d33f22ab1968f215
Phoomparin Mano <phoomparin@gmail.com>
2019-09-08 11:40:05 +0700

    :sparkles: In-memory /users

---
+++ src/hello.ts
@@ -0,0 +1,13 @@
+console.log("Hello")
+console.log(5 * 5)
+
+// Create new file -> hello.ts
+// > Quokka Start on Current File
+
+function join(list: string[]): string {
+  return list.join('')
+}
+
+console.log(join(['a', 'b']))
+
+
